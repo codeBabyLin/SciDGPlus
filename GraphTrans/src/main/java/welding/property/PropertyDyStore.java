a88b72730cb717e95a4de171beabe57f33e73af1
codeBabyLin <zengchenglin@cnic.cn>
2023-10-13 12:12:40 +0800

    add gradoop

---
+++ GraphTrans/src/main/java/welding/property/PropertyDyStore.java
@@ -0,0 +1,14 @@
+package welding.property;
+
+import KVStore.RocksDBStorage;
+import welding.Transformer;
+
+public class PropertyDyStore {
+    private NameStore nameStore;
+    private RocksDBStorage db;
+    private Transformer transformer;
+    public PropertyDyStore(String dir){
+
+    }
+
+}
